74993f6164b6cb7be116c221e6fdb40858937307
Zhuolithon <zw2536@columbia.edu>
2019-10-12 16:25:16 -0400

    Q5

--- Bashcode.sh
+++ Bashcode.sh
@@ -11,3 +11,6 @@ sort -k 1 -r bezdekIris.data > Q2_sort.txt
 
 # head with an argument to view the top 25 entries
 head -n 25 bezdekIris.data > Q2_head.txt
+
+# cut to then extract only the class (which is now the fifth field)
+cut -d ',' -f 5 bezdekIris.data > Q2_cut_class.txt
