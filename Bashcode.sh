df4ebb4588caa0561d412c0e48bf9f5c3201a3dd
Zhuolithon <zw2536@columbia.edu>
2019-10-12 16:38:32 -0400

    Bash script

--- Bashcode.sh
+++ Bashcode.sh
@@ -7,10 +7,16 @@ curl -s -O https://archive.ics.uci.edu/ml/machine-learning-databases/iris/bezdek
 cut -d ',' -f 1,5 bezdekIris.data > Q2_cut.txt
 
 # sort to sort the data by sepal length starting with the largest
-sort -k 1 -r bezdekIris.data > Q2_sort.txt
+sort -t ',' -k 1 -r Q2_cut.txt > Q2_sort.txt
 
 # head with an argument to view the top 25 entries
-head -n 25 bezdekIris.data > Q2_head.txt
+head -n 25 Q2_sort.txt > Q2_head.txt
 
-# cut to then extract only the class (which is now the fifth field)
-cut -d ',' -f 5 bezdekIris.data > Q2_cut_class.txt
+# cut to then extract only the class (which is now the 2nd field)
+cut -d ',' -f 2 Q2_head.txt > Q2_cut_class.txt
+
+# sort the class data lexicographicly
+sort -t ',' -k5 -d Q2_cut_class.txt > Q2_sort_lexicographically.txt
+
+# count how many of each class are in the largest 25 sepal length
+uniq -c Q2_sort_lexicographically.txt > Q2_uniq.txt
